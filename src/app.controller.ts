a47935b8180aa272637b0e2b51dd1efe75ce2c5c
Sergey Petrov <mavericusdev@gmail.com>
2022-07-13 11:38:16 +0300

    feat: add endpoint title

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('title')
+  getTitle(): string {
+    return this.appService.getTitle();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getTitle(): string {
+    return 'Finance App';
+  }
 }
